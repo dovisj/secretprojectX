b9959a5008d11d05891cfb7b0589fe46e08f431a
Bo <zboyang@umich.edu>
2020-07-10 23:28:18 -0400

    player movement and AI controls

---
+++ Assets/Scripts/Player/Controller_AI.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_AI : MonoBehaviour
+{
+    [SerializeField] private Controller_Player script_controller_player = null;
+
+    [SerializeField] private string tag_seek = "";
+
+    [SerializeField] private float ai_speed_multiplier = 1f;
+
+    /*************************************************/
+
+    private GameObject current_target = null;
+    private float speed = 0;
+    private bool ai_control = false;
+
+    private void Start()
+    {
+        speed = script_controller_player.GetSpeed() * ai_speed_multiplier;
+    }
+
+    private void Update()
+    {
+        ai_control = script_controller_player.GetAIControl();
+
+        if (!ai_control)
+        {
+            current_target = null;
+            return;
+        }
+        
+        // Find new interaction target 
+        if (current_target == null)
+        {
+            // Get all possible objects
+            GameObject[] seek_objects = GameObject.FindGameObjectsWithTag(tag_seek);
+            
+            // Randomly choose an object to interact with
+            int seek_index = (int)((float) seek_objects.Length * Random.value);
+            current_target = seek_objects[seek_index];
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!ai_control)
+        {
+            return;
+        }
+
+        Vector2 seek_pos = current_target.transform.position;
+
+        Debug.DrawLine(transform.position, seek_pos);
+        transform.position = Vector2.MoveTowards(transform.position, seek_pos, speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!ai_control)
+        {
+            return;
+        }
+
+        if (current_target == null)
+        {
+            return;
+        }
+
+        if (collision.collider.gameObject.GetInstanceID() == current_target.GetInstanceID())
+        {
+            // Do interaction
+
+            // Reset target
+            current_target = null;
+        }
+        else if (collision.collider.tag == tag_seek)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Controller_Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Player : MonoBehaviour
+{
+    [SerializeField] private float move_speed = 0f;
+    [SerializeField] private float time_human_control_seconds = 0f;
+    [SerializeField] private float time_ai_control_seconds = 0f;
+
+    /*************************************************/
+
+    private Rigidbody2D ref_rbody = null;
+    private SpriteRenderer ref_sprite_renderer = null;
+
+    private float input_h_axis = 0f;
+    private float input_v_axis = 0f;
+    private float last_control_change_time = 0f;
+    private bool ai_control = false;
+
+    public float GetSpeed() { return move_speed;}
+    public bool GetAIControl() { return ai_control; }
+
+    private void Awake()
+    {
+        ref_rbody = this.GetComponent<Rigidbody2D>();
+        ref_sprite_renderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        last_control_change_time = Time.time;
+    }
+
+    private void Update()
+    {
+        // Check if it's time to change control modes
+        float elapsed_time = Time.time - last_control_change_time;
+        if (elapsed_time > (ai_control ? time_ai_control_seconds : time_human_control_seconds))
+        {
+            // Swap to the other
+            ai_control = !ai_control;
+            last_control_change_time = Time.time;
+
+            // Reset velocity
+            ref_rbody.velocity = new Vector2(0f, 0f);
+
+            // Visual differentiation
+            if (ai_control)
+            {
+                ref_sprite_renderer.color = Color.black;
+            }
+            else
+            {
+                ref_sprite_renderer.color = Color.white;
+            }
+        }
+
+        input_h_axis = Input.GetAxisRaw("Horizontal");
+        input_v_axis = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (ai_control)
+        {
+            return;
+        }
+
+        float vel_x = input_h_axis * move_speed * Time.fixedDeltaTime;
+        float vel_y = input_v_axis * move_speed * Time.fixedDeltaTime;
+
+        ref_rbody.velocity = new Vector2(vel_x, vel_y);
+    }
+}
