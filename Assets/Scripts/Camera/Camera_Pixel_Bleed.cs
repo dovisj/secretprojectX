5f57b8e76f6ee0955a9670471e7896610e252a36
Bo <zboyang@umich.edu>
2020-07-11 18:48:40 -0400

    Fixed pixel bleed issue on tileset

---
+++ Assets/Scripts/Camera/Camera_Pixel_Bleed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Pixel_Bleed : MonoBehaviour
+{
+    // Pixel bleeding occurs when camera resolution is non-integer
+
+    [SerializeField] private Camera ref_camera = null;
+
+    [SerializeField] private float snap_x = 1f;
+    [SerializeField] private float snap_y = 1f;
+
+    private void Update()
+    {
+        Vector2 new_size = new Vector2(Snap(Screen.width, snap_x), Snap(Screen.height, snap_y));
+        ref_camera.pixelRect = new Rect(ref_camera.pixelRect.position, new_size);
+    }
+
+    private float Snap(float x, float snap_size)
+    {
+        return Mathf.Floor(x / snap_size) * snap_size;
+    }
+}
